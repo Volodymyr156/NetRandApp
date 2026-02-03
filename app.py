adc8ef6b6ee406db63146d864609d2ecf2bd0802
V156 <utkaduck156@duck.com>
2026-02-03 01:15:29 +0100

    Update file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from func import link_gen, iplink_gen
+from func import link_gen, nums_gen
 #Main Menu
 import os
 
@@ -18,48 +18,56 @@ For further information, type 'HELP' in your terminal. :)
 The input is not case-sensitive.""")
 
 i = "10"
-user_folder = "./found_urls"
+user_path = "./found_urls"
 user_w_urls = "working_urls.txt"
 user_b_urls = "bad_urls.txt"
 
 
 while True:
     user_input = input()
-
     if user_input.upper() == "HELP":
         print("""
-        HINT: see some hints
+        GUIDE: see the detailed explanation
+        ITER: input the number of iterations
+        FILE: input these values: <path to store files> <filename for working urls> <filename for bad urls>
         LINK: input these values: <protocol> <number of random chars> <number of random numbers> <domain> and generate
-        IPLINK: input these values: <protocol> 4x<number of random numbers> <domain> and generate
-        #CLEAR: clear the terminal
+        NUMS: input the <protocol> and generate
         EXIT: close the program
         """)
 
-    elif user_input.upper() == "HINT":
-        print("""HINTS:
--
+    elif user_input.upper() == "GUIDE":
+        print("""There will be something "_"
 """)
 
+    elif user_input.upper() == "ITER":
+        i = input("(ITER)Your input:")
+
+    elif user_input.upper() == "FILE":
+        user_file = input("(FILE)Your input: ")
+        user_file = user_file.split()
+        user_folder = user_file[0]
+        user_w_urls = user_file[1]
+        user_b_urls = user_file[2]
+
     elif user_input.upper() == "LINK":
-        link_input = input("Your input(space is the delimiter): ")
+        link_input = input("(LINK)Your input: ")
         link_input = link_input.split()
-        if len(link_input) < 5:
+        if len(link_input) < 1:
             print("Not enough arguments")
         else:
             try:
-                link_gen(link_input)
+                link_gen(link_input+[i])
             except Exception as e:
                 print(f"Something went wrong: {e}")
 
-    elif user_input.upper() == "IPLINK":
-        iplink_input = input("Your input(space is the delimiter): ")
-        iplink = iplink_input.split()
-        if len(iplink) < 6:
+    elif user_input.upper() == "NUMS":
+        nums_input = input("(NUMS)Your input: ")
+        nums_input = nums_input.split()
+        if len(nums_input) < 1:
             print("Not enough arguments")
         else:
-
             try:
-                iplink_gen(iplink_input)
+                nums_gen(nums_input+[i])
             except Exception as e:
                 print(f"Something went wrong: {e}")
 
--- func.py
+++ func.py
@@ -1,3 +1,5 @@
+# from app import user_path, user_w_urls, user_b_urls
+
 import random
 import requests
 
@@ -10,7 +12,8 @@ list_chars = [chr(x) for x in range(97,123)]
 # str_symbols = "~._-"
 # with open("dictionary.json", "r") as file:
 #     english_dictionary = json.load(file)
-
+def create_files():
+    pass
 
 
 def link_gen(link_input):
@@ -20,8 +23,7 @@ def link_gen(link_input):
     domain = link_input[3].lower()
     i = int(link_input[4])
 
-    print(protocol,k_chars,k_numbers,domain)
-
+    print(protocol,k_chars,k_numbers,domain,i)
     while i > 0:
         loop_chars = random.sample(list_chars, k= k_chars)
         loop_numbers = random.sample(list_numbers, k=k_numbers)
@@ -38,16 +40,35 @@ def link_gen(link_input):
                 print(f"Working URL: ************{generated_url}************")
                 code_200.append(generated_url)
                 i -= 1
-
-
             except:
                 print(f"Bad Url: {generated_url}")
                 code_bad.append(generated_url)
-
     print(f"Loop completed")
 
 
-def iplink_gen(iplink_input):
-    pass
+def nums_gen(nums_input):
+    protocol = nums_input[0].lower()
+    i = int(nums_input[1])
 
+    while i > 0:
+        loop_num1 = random.sample(list_numbers, k=random.randrange(1,4))
+        loop_num2 = random.sample(list_numbers, k=random.randrange(1,4))
+        loop_num3 = random.sample(list_numbers, k=random.randrange(1,4))
+        loop_num4 = random.sample(list_numbers, k=random.randrange(1,4))
+
+        loop_nums = ["".join(loop_num1)] + ["".join(loop_num2)] + ["".join(loop_num3)] + ["".join(loop_num4)]
+
+        str_nums = ".".join(loop_nums)
+
+        generated_url = protocol + str_nums
+        if generated_url not in code_200 and generated_url not in code_bad:
+            try:
+                request = requests.get(generated_url)
+                print(f"Working URL: ************{generated_url}************")
+                code_200.append(generated_url)
+                i -= 1
+            except:
+                print(f"Bad Url: {generated_url}")
+                code_bad.append(generated_url)
+    print(f"Loop completed")
 
